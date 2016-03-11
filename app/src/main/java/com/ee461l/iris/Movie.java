b20f832fc05459449e7190be6864cd7254fed614
JCai2017 <jason.cai.plano@gmail.com>
2016-03-11 14:22:59 -0600

    Added Login capability using Google Sign-in. Added RegisteredUser and empty Movie classes. (Still need to be completed)

---
+++ app/src/main/java/com/ee461l/iris/Movie.java
@@ -0,0 +1,7 @@
+package com.ee461l.iris;
+
+/**
+ * Created by Jason Cai 3/11/2016.
+ */
+public class Movie {
+}
---
+++ app/src/main/java/com/ee461l/iris/RegisteredUser.java
@@ -0,0 +1,45 @@
+package com.ee461l.iris;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jason Cai on 3/11/2016.
+ */
+public class RegisteredUser {
+    private String ID;
+    private String password;
+    private ArrayList<Movie> ratedMovies = new ArrayList<Movie>();
+    private ArrayList<Integer> ratings = new ArrayList<Integer>();
+    private ArrayList<String> likedGenres = new ArrayList<String>();
+    private ArrayList<Integer> genreRatings = new ArrayList<Integer>();
+
+    private RegisteredUser(){}
+
+    public RegisteredUser(String ID, String password){
+        this.ID = ID;
+        this.password = password;
+    }
+
+    public String getEmail(){
+        return ID;
+    }
+
+    public boolean login(String pass){
+        return this.password.equals(pass);
+    }
+
+    public void rateMovie(String title, int rating){
+        if(ratedMovies.contains(title)){
+           //return;
+        }else{
+            Movie movie = new Movie();
+            ratedMovies.add(movie);
+            ratings.add(rating);
+        }
+    }
+
+    public void recommend(){
+
+    }
+
+}
