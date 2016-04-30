2e2cfde254a73990ca15120846d4a3999fb95b44
sbhadsavle <sbhadsavle@tacc.utexas.edu>
2016-04-29 23:51:41 -0500

    Fixed the code for finding the location and zipcode

--- app/src/main/java/com/ee461l/iris/ExploreMovieActivity.java
+++ app/src/main/java/com/ee461l/iris/ExploreMovieActivity.java
@@ -1,6 +1,9 @@
 package com.ee461l.iris;
 
+import android.content.pm.PackageManager;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +11,7 @@ import android.os.Bundle;
 public class ExploreMovieActivity extends AppCompatActivity {
 
     private static BoxOfficeMovie theMovie;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +31,24 @@ public class ExploreMovieActivity extends AppCompatActivity {
 
         // assign the movie. NOTE: you need to manually load it. It doesn't load here
         theMovie = (BoxOfficeMovie) getIntent().getSerializableExtra(BoxOfficeActivity.MOVIE_DETAIL_KEY);
+
+        // Here, thisActivity is the current activity
+        if ( checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+                // No explanation needed, we can request the permission.
+
+            requestPermissions(new String[] {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_ASK_PERMISSIONS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+        }
     }
 
     public static BoxOfficeMovie getTheMovie(){
         return theMovie;
     }
+
+
 }
